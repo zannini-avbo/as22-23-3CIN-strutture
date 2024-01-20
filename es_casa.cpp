79999257b04b624b68946452502e0bd8d6bbb3af
Matteo Zannini <144007002+zannini-avbo@users.noreply.github.com>
2024-01-20 11:12:35 +0000

    update on .gitignore

---
+++ es_casa.cpp
@@ -0,0 +1,16 @@
+/*
+Creare due array di tipo "string" di 10 elementi: uno chiamato nome e uno cognome
+Caricare i vettori con i nomi i cognomi di 10 studenti.
+Dare un errore se l'utente prova ad inserire l'undicesimo studente.
+
+Creare un menu con le seguenti opzioni:
+    stampare le informazioni degli studenti dal primo al ultimo studente inserito
+    stampare le informazioni degli studenti del ultimo al primo studente inserito
+    uscire dal menu se l'utente inserisce "esci"
+*/
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
