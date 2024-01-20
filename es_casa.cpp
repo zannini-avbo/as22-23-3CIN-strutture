604c32678c9993c1f279f896579becf3d057610b
Matteo Zannini <144007002+zannini-avbo@users.noreply.github.com>
2024-01-20 11:47:31 +0000

    es gallo

--- es_casa.cpp
+++ es_casa.cpp
@@ -1,12 +1,8 @@
 /*
-Creare due array di tipo "string" di 10 elementi: uno chiamato nome e uno cognome
-Caricare i vettori con i nomi i cognomi di 10 studenti.
-Dare un errore se l'utente prova ad inserire l'undicesimo studente.
-
-Creare un menu con le seguenti opzioni:
-    stampare le informazioni degli studenti dal primo al ultimo studente inserito
-    stampare le informazioni degli studenti del ultimo al primo studente inserito
-    uscire dal menu se l'utente inserisce "esci"
+Rubrica con strutture dati". 
+Il programma deve caricare due vettori di stringhe con i nomi e i cognomi di dieci persone
+che vengono chiesti all'utente 
+e alla finme del caricamento stampa la rubrica dei nomi e cognomi in ordine inverso a come sono stati caricati.
 */
 
 #include <iostream>
